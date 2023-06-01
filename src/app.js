de77db6900bae6f6b5375e949ac9894afce4f3b1
Khrise Franchesca <129009072+Khrisefzm@users.noreply.github.com>
2023-06-01 16:34:13 +0000

    Add JavaScript javascript in the html using a <p> element

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//Basic solution
 window.onload = function() {
   //write your code here
   let pronoun = ["the", "our"];
@@ -19,3 +20,33 @@ window.onload = function() {
     }
   }
 };
+
+//Write diferents name of the domain in the web page
+let newPronoun = ["the", "our"];
+let newAdj = ["great", "big"];
+let newNoun = ["jogger", "racoon"];
+
+function generateName(firstArray, secondArray, thirdArray) {
+  let nameOfDomain = [];
+  for (let i = 0; i < firstArray.length; i++) {
+    for (let j = 0; j < secondArray.length; j++) {
+      for (let k = 0; k < thirdArray.length; k++) {
+        nameOfDomain.push(
+          `${firstArray[i]}${secondArray[j]}${thirdArray[k]}.com`
+        );
+      }
+    }
+  }
+  return nameOfDomain;
+}
+
+document.getElementById("domainNameGenerator").innerHTML = generateName(
+  newPronoun,
+  newAdj,
+  newNoun
+);
+document.getElementById("numberOfDomains").innerHTML = generateName(
+  newPronoun,
+  newAdj,
+  newNoun
+).length;
